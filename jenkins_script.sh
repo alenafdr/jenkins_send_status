bc9ac296b95dad0934d8b67b8b4b1cde0b71c23f
agaidt <agaidt@lineate.com>
2019-08-30 13:41:31 +0600

    Added description in readme

--- jenkins_script.sh
+++ jenkins_script.sh
@@ -4,7 +4,7 @@ login=$1
 password=$2
 jenkins_server=$3
 project=$4
-slcahook_url=$5
+slackhook_url=$5
 json=$(curl "http://${login}:${password}@${jenkins_server}/job/${project}/lastBuild/api/json")
 
 regex='"result":"([^"]+)'
@@ -12,7 +12,7 @@ regex='"result":"([^"]+)'
 [[ $json =~ $regex ]]
 text="Project - ${project} \nStatus last build - ${BASH_REMATCH[1]}"
 
-curl -X POST -H 'Content-type: application/json' --data '{"text":"'"${text}"'"}' "${slcahook_url}"
+curl -X POST -H 'Content-type: application/json' --data '{"text":"'"${text}"'"}' "${slackhook_url}"
 
 exit 0
 
